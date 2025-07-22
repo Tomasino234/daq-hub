3abd1c9b8657ac34756e6e3ac467729392f073c0
Tomasino234 <tomas.strezo@gmail.com>
2025-07-22 22:11:54 +0200

    ding dong ititap

--- loader.lua
+++ loader.lua
@@ -1,5 +1,4 @@
-local loading_mod = loadstring(game:HttpGet("https://raw.githubusercontent.com/Tomasino234/daq-hub/refs/heads/main/main/loading.lua?token=GHSAT0AAAAAADHTSVTTDYCOF2D7L4GTFQRG2D735CQ"))()
-local bypass_mod = loadstring(game:HttpGet("https://raw.githubusercontent.com/Tomasino234/daq-hub/refs/heads/main/main/bypass.lua?token=GHSAT0AAAAAADHTSVTTXF4LES723FLRTT222D74C6A"))()
-
+local loading_mod = loadstring(game:HttpGet("https://raw.githubusercontent.com/Tomasino234/daq-hub/refs/heads/main/main/loading.lua"))()
+local bypass_mod = loadstring(game:HttpGet("https://raw.githubusercontent.com/Tomasino234/daq-hub/refs/heads/main/main/bypass.lua"))()
 loading_mod.init()
 bypass_mod.init()

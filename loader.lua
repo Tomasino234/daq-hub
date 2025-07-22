d524ce74f4c18a9641844861bc79349aa0238604
Tomasino234 <tomas.strezo@gmail.com>
2025-07-22 21:04:19 +0200

    dwa

--- loader.lua
+++ loader.lua
@@ -1,5 +1,5 @@
 local loading_mod = loadstring(game:HttpGet("https://raw.githubusercontent.com/Tomasino234/daq-hub/refs/heads/main/main/loading.lua?token=GHSAT0AAAAAADHTSVTTDYCOF2D7L4GTFQRG2D735CQ"))()
-local bypass_mod = loadstring(game:HttpGet("https://raw.githubusercontent.com/Tomasino234/daq-hub/refs/heads/main/main/bypass.lua?token=GHSAT0AAAAAADHTSVTSZGCQT42HUGIBF6QK2D734IA"))()
+local bypass_mod = loadstring(game:HttpGet("https://raw.githubusercontent.com/Tomasino234/daq-hub/refs/heads/main/main/bypass.lua?token=GHSAT0AAAAAADHTSVTTXF4LES723FLRTT222D74C6A"))()
 
 loading_mod.init()
 bypass_mod.init()
